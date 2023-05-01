a2b816a12eb8713eeffe65d9cedaf975cf2fa0bd
kamruzzaman22874 <joybd.eee@gmail.com>
2023-05-01 18:35:10 +0600

    added hotel rooms data

--- index.js
+++ index.js
@@ -5,28 +5,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
+const hotels = require('./data/hotel.json')
 const places = require('./data/travel-data.json')
+
+console.log(hotels);
 
 app.get('/', (req, res) => {
     res.send('Travel guru server is running')
 })
 
-app.get('/places', (req, res)=>{
+app.get('/places', (req, res) => {
     res.send(places)
 })
-
 app.get('/places/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const categoryInfo = places.destinations.find(info => parseInt(info.id) === id);
     res.send(categoryInfo)
-    
+
 })
 
-app.get('/booking/:id', (req, res) => {
+app.get('/hotels', (req, res) => {
+    res.send(hotels)
+})
+app.get('/hotels/:id', (req, res) => {
     const bookingId = parseInt(req.params.id)
-    const bookingInfo = places.destinations.find(booking => parseInt(booking.id) === bookingId)
+    const bookingInfo = hotels.find(booking => parseInt(booking.id) === bookingId)
+    console.log(bookingInfo);
     res.send(bookingInfo)
 })
+
+
 
 
 app.listen(port, () => {
